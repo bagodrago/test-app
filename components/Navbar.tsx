0e709f76bf2d934c394a5318ca3093b8168b7b9c
David Howe <65414349+bagodrago@users.noreply.github.com>
2025-10-19 14:54:54 -0400

    Added prettier plugin to improve readability

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,51 +1,43 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import {Menu, X} from "lucide-react";
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+import { Menu, X } from "lucide-react"
 
-import { useMenu } from "@/context/MenuContext";
+import { useMenu } from "@/context/MenuContext"
 
 const Navbar = () => {
-  const {menuOpen, setMenuOpen} = useMenu();
-  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const { menuOpen, setMenuOpen } = useMenu()
+  const toggleMenu = () => setMenuOpen(!menuOpen)
 
   return (
     <nav className="navbar">
       <div className="navbar__frame">
-
-        <Link 
-          href="#top"
-        >
-          <Image
-          src="/logotk.svg"
-          alt="David Howe"
-          width={130}
-          height={80}
-          />
+        <Link href="#top">
+          <Image src="/logotk.svg" alt="David Howe" width={130} height={80} />
         </Link>
 
         {/* Desktop Links */}
-        <div className="hidden md:flex flex-row items-center space-x-10">
+        <div className="hidden flex-row items-center space-x-10 md:flex">
           <Link href="#about" className="navbar__item">
             About
           </Link>
-          
+
           <Link href="#projects" className="navbar__item">
             Projects
           </Link>
-          
+
           <Link href="#contact" className="navbar__item">
             Contact Me
           </Link>
         </div>
-        
+
         {/* Hamburger Menu for Mobile */}
         <button
           onClick={toggleMenu}
           className="navbar__hamburger"
           aria-label="Toggle menu"
         >
-          {menuOpen ? <X size={28}/> : <Menu size={28}/>}
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
         </button>
       </div>
     </nav>
