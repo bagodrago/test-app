b0f480f141df001eeafdb1ce8583d5b61f97f1bb
David Howe <65414349+bagodrago@users.noreply.github.com>
2026-02-26 10:37:59 -0500

    Removed deprecated files

--- app/components.ts
+++ app/components.ts
@@ -1,11 +1,9 @@
-import Header from './components/Header';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import About from './components/About'
 import DropdownMenu from './components/DropdownMenu';
 
 export {
-    Header,
     Hero,
     Navbar,
     About,
